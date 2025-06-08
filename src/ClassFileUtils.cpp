75251c232fde267baf31cc703dfc1c5a82680a54
Marcos <marcosp.oliveir4@gmail.com>
2025-06-08 19:12:10 -0300

    voltei para versao do c+11

--- src/ClassFileUtils.cpp
+++ src/ClassFileUtils.cpp
@@ -1,5 +1,4 @@
 #include "ClassFileUtils.hpp"
-#include <filesystem>
 
 using namespace std;
 
@@ -39,11 +38,29 @@ bool ClassFileUtils::compare_utf8_str(ConstUtf8Info constant, const char* str)
 
 bool ClassFileUtils::verify_classfile_name(string filename, string classname)
 {
-    // std::filesystem::path lida com / e \ automaticamente.
-    std::string base_filename = std::filesystem::path(filename).stem().string();
-    std::string base_classname = std::filesystem::path(classname).stem().string();
-
-    return base_classname == base_filename;
+    int aux_pos;
+    filename = filename.substr(0, filename.size() - 6);
+    aux_pos = filename.find("\\");
+    while (aux_pos >= 0 && (unsigned int)aux_pos <= filename.size()) {
+        filename = filename.substr(aux_pos + 1);
+        aux_pos = filename.find("\\");
+    }
+    aux_pos = filename.find("/");
+    while (aux_pos >= 0 && (unsigned int)aux_pos <= filename.size()) {
+        filename = filename.substr(aux_pos + 1);
+        aux_pos = filename.find("/");
+    }
+    aux_pos = classname.find("\\");
+    while (aux_pos >= 0 && (unsigned int)aux_pos <= classname.size()) {
+        classname = classname.substr(aux_pos + 1);
+        aux_pos = classname.find("\\");
+    }
+    aux_pos = classname.find("/");
+    while (aux_pos >= 0 && (unsigned int)aux_pos <= classname.size()) {
+        classname = classname.substr(aux_pos + 1);
+        aux_pos = classname.find("/");
+    }
+    return (classname == filename);
 }
 
 const char* ClassFileUtils::stream_to_cstring(const stringstream& ss)
